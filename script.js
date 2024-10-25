f71c11683138fb24149fe1c6d095f3852e3b9a42
Constantine Konovalov <constantine.konovalov@gmail.com>
2024-10-25 22:39:36 +0300

    Ваще

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputText = document.getElementById('inputText');
     const outputText = document.getElementById('outputText');
+    const copyButton = document.getElementById('copyButton');
 
     // Загрузка сохраненного текста из localStorage
     const savedText = localStorage.getItem('hebrewText');
@@ -16,6 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('hebrewText', inputText.value);
     });
 
+    // Функционал копирования
+    copyButton.addEventListener('click', () => {
+        const textToCopy = outputText.innerText;
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            // Эффект подмигивания
+            outputText.classList.add('blink');
+            setTimeout(() => {
+                outputText.classList.remove('blink');
+            }, 1000); // Изменено с 500 на 1000 мс
+        });
+    });
+
     function updateOutput() {
         const input = inputText.value;
         const reversed = reverseHebrewText(input);
@@ -23,54 +36,55 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function reverseHebrewText(text) {
-        const hebrewChars = /[\u0590-\u05FF]/;
-        const nonReversibleChars = /[0-9a-zA-Z:]/;
-        const punctuation = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/;
-        const brackets = /[\(\)\[\]\{\}]/;
-        
+        const lines = text.split('\n');
         let result = '';
-        let currentWord = '';
-        let nonReversibleSequence = '';
-        
-        for (let i = text.length - 1; i >= 0; i--) {
-            const char = text[i];
-            if (hebrewChars.test(char)) {
-                if (nonReversibleSequence) {
-                    currentWord = nonReversibleSequence + currentWord;
+
+        for (let line of lines) {
+            const hebrewChars = /[\u0590-\u05FF]/;
+            const nonReversibleChars = /[0-9a-zA-Z:.]/;
+            const punctuation = /[!@#$%^&*()_+\-=\[\]{};\\|,<>/?"""']/;
+            const brackets = /[\(\)\[\]\{\}]/;
+            
+            let lineResult = '';
+            let currentWord = '';
+            let nonReversibleSequence = '';
+            
+            for (let i = line.length - 1; i >= 0; i--) {
+                const char = line[i];
+                if (hebrewChars.test(char)) {
+                    lineResult += nonReversibleSequence + currentWord;
+                    currentWord = createCharSpan(char);
                     nonReversibleSequence = '';
-                }
-                currentWord += createCharSpan(char);
-            } else if (nonReversibleChars.test(char)) {
-                nonReversibleSequence = createCharSpan(char, true) + nonReversibleSequence;
-            } else if (brackets.test(char)) {
-                const reversedBracket = reverseBracket(char);
-                if (currentWord || nonReversibleSequence) {
-                    result += nonReversibleSequence + currentWord + createCharSpan(reversedBracket, true);
+                } else if (nonReversibleChars.test(char)) {
+                    nonReversibleSequence = createCharSpan(char, true) + nonReversibleSequence;
+                } else if (brackets.test(char)) {
+                    const reversedBracket = reverseBracket(char);
+                    lineResult += nonReversibleSequence + currentWord + createCharSpan(reversedBracket, true);
                     currentWord = '';
                     nonReversibleSequence = '';
-                } else {
-                    result += createCharSpan(reversedBracket, true);
+                } else if (punctuation.test(char)) {
+                    lineResult += nonReversibleSequence + currentWord + createCharSpan(char, true);
+                    currentWord = '';
+                    nonReversibleSequence = '';
+                } else if (char === ' ') {
+                    lineResult += nonReversibleSequence + currentWord + createCharSpan(' ');
+                    currentWord = '';
+                    nonReversibleSequence = '';
                 }
-            } else if (punctuation.test(char)) {
-                if (nonReversibleSequence) {
-                    nonReversibleSequence = createCharSpan(char, true) + nonReversibleSequence;
-                } else {
-                    currentWord = createCharSpan(char, true) + currentWord;
-                }
-            } else if (char === ' ') {
-                result += nonReversibleSequence + currentWord + createCharSpan(' ');
-                currentWord = '';
-                nonReversibleSequence = '';
             }
+            
+            lineResult += nonReversibleSequence + currentWord;
+            result += lineResult + '<br>';
         }
         
-        result += nonReversibleSequence + currentWord;
-        
-        return result;
+        return result.trim();
     }
 
     function createCharSpan(char, highlight = false) {
-        const className = highlight ? 'highlight' : '';
+        let className = '';
+        if (highlight) {
+            className = /[0-9a-zA-Z]/.test(char) ? 'highlight-dark' : 'highlight';
+        }
         return `<span class="${className}">${char}</span>`;
     }
 
